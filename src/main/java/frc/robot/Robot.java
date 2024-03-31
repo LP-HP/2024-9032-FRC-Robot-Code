cc1b95bdd77533a85499577983f1d852d4785691
LP-HP <game477777@gmail.com>
2024-03-31 14:41:42 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,7 +26,11 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    robotContainer = new RobotContainer();
+    try {
+      robotContainer = new RobotContainer();
+    } catch (InterruptedException e) {
+      
+    }
   }
 
   /**
